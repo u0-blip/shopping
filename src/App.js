67ed287baab5dfd8dd041e0717e384dd0d4d44fc
u0-blip <chenduona@gmail.com>
2020-09-12 15:22:58 +1000

    skeleton code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import ProductList from './ProductList/ProductList';
+import Details from './Details/Details';
+import Cart from './Cart/Cart';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar>
+        <Switch>
+          <Route exact path='/' component={ProductList}/>
+          <Route exact path='/details' component={Details}/>
+          <Route exact path='/cart' component={Cart}/>
+        </Switch>
+      </Navbar>
+    </Fragment>
   );
 }
 
